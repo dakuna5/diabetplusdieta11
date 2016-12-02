7c3812496e3a0b00a16e29d8bc9ada065a653682
alexey <macbook@mac-mini-alexey>
2016-12-03 00:42:16 +0200

    Initial commit

---
+++ Untitled/APHomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  APHomeVC.swift
+//  Untitled
+//
+//  Created by alexeyweb11@gmail.com on 02/12/2016, using AnimaApp.com, under MIT license.
+//  Copyright © 2016 Company Name. All rights reserved.
+//
+
+import UIKit
+
+public class APHomeVC : ANViewController {
+    
+
+    
+
+}
---
+++ Untitled/APTabBarScreen1OverlayVC.swift
@@ -0,0 +1,18 @@
+//
+//  APTabBarScreen1OverlayVC.swift
+//  Untitled
+//
+//  Created by alexeyweb11@gmail.com on 02/12/2016, using AnimaApp.com, under MIT license.
+//  Copyright © 2016 Company Name. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+public class APTabBarScreen1OverlayVC : ANViewController, ANTabBarControllerOverlay  {
+    
+    @IBOutlet weak var map1: MKMapView?
+
+    
+
+}
